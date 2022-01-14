274fe64c8e8ed8cfb4b3d9474e4edb69c6503d9f
Dimitar Kupanov <dimitarkupanov@gmail.com>
2022-01-14 22:31:39 +0200

    Extract a server login in separate class.

---
+++ WebServer.Server/HttpServer.cs
@@ -0,0 +1,88 @@
+﻿namespace WebServer.Server
+{
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;
+    using HTTP;
+
+    public class HttpServer
+    {
+        private readonly IPAddress ipAddress;
+        private readonly int port;
+        private readonly TcpListener serverListener;
+
+        public HttpServer(string ipAddress, int port)
+        {
+            this.ipAddress = IPAddress.Parse(ipAddress);
+            this.port = port;
+
+            this.serverListener = new TcpListener(this.ipAddress, this.port);
+        }
+
+        public void Start()
+        {
+            this.serverListener.Start();
+
+            Console.WriteLine($"Server started on port {this.port}.");
+            Console.WriteLine("Listening for requests...");
+
+            while (true)
+            {
+                var connection = serverListener.AcceptTcpClient();
+
+                var networkStream = connection.GetStream();
+
+                string requestText = ReadRequest(networkStream);
+
+                Console.WriteLine(requestText);
+
+                WriteResponse(networkStream, "Hello from the server!");
+
+                connection.Close();
+            }
+
+        }
+
+        private void WriteResponse(NetworkStream networkStream, string message)
+        {
+            var contentLength = Encoding.UTF8.GetByteCount(message);
+
+            var response = $@"HTTP/1.1 200 OK
+Content-Type: text/plain; charset=UTF-8
+Content-Length: {contentLength}
+
+{message}";
+
+            var responseBytes = Encoding.UTF8.GetBytes(response);
+
+            networkStream.Write(responseBytes);
+        }
+
+        private string ReadRequest(NetworkStream networkStream)
+        {
+            var bufferLength = 1024;
+            var buffer = new byte[bufferLength];
+
+            var totalBytes = 0;
+
+            StringBuilder requestBuilder = new StringBuilder();
+
+            do
+            {
+                var bytesRead = networkStream.Read(buffer, 0, bufferLength);
+
+                totalBytes += bytesRead;
+
+                if (totalBytes > 10 * 1024)
+                {
+                    throw new InvalidOperationException("Request is too large.");
+                }
+
+                requestBuilder.Append(Encoding.UTF8.GetString(buffer, 0, bytesRead));
+
+            } while (networkStream.DataAvailable); //May not run correctly over the Internet
+
+            return requestBuilder.ToString();
+        }
+    }
+}
