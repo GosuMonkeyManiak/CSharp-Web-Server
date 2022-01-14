4aa73ba9ae41c9c8fb68187bc0b3d710f1594155
Dimitar Kupanov <dimitarkupanov@gmail.com>
2022-01-14 22:34:10 +0200

    Web server little demo

---
+++ WebServer.Demo/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace WebServer.Demo
+{
+    using Server;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            HttpServer server = new HttpServer("127.0.0.1", 8080);
+            server.Start();
+        }
+    }
+}
