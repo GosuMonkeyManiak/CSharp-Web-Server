8d238e467c2ebbef9fe7448500d53c00983441a7
Dimitar Kupanov <dimitarkupanov@gmail.com>
2022-01-17 09:18:35 +0200

    Try and test the web server

--- WebServer.Demo/StartUp.cs
+++ WebServer.Demo/StartUp.cs
@@ -1,13 +1,35 @@
 ﻿namespace WebServer.Demo
 {
     using Server;
+    using Server.HTTP;
+    using Server.Responses;
 
     public class StartUp
     {
-        static void Main(string[] args)
+        private const string HtmlForm = @"<form action='/HTML' method='POST'>
+               Name: <input type='text' name='Name'/>
+               Age: <input type='number' name ='Age'/>
+               <input type='submit' value ='Save' />
+            </form>";
+
+
+        public static void Main(string[] args)
+            => new HttpServer(routes => routes
+                .MapGet("/", new TextResponse("Hello from the server."))
+                .MapGet("/Redirect", new RedirectResponse("https://softuni.bg"))
+                .MapGet("/HTML", new HtmlResponse(StartUp.HtmlForm))
+                .MapPost("/HTML", new TextResponse("", StartUp.AddFormDataAction)))
+            .Start();
+
+        private static void AddFormDataAction(Request request, Response response)
         {
-            HttpServer server = new HttpServer("127.0.0.1", 8080);
-            server.Start();
+            response.Body = "";
+
+            foreach (var (key, value) in request.Form)
+            {
+                response.Body = $"{key} - {value}";
+                response.Body = Environment.NewLine;
+            }
         }
     }
 }
