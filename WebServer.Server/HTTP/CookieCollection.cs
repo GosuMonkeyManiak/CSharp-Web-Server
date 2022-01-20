f430dc83288c9bdead0c94b8339986e1ff0a44b2
Dimitar Kupanov <dimitarkupanov@gmail.com>
2022-01-20 21:45:06 +0200

    Include a Count for easy use

--- WebServer.Server/HTTP/CookieCollection.cs
+++ WebServer.Server/HTTP/CookieCollection.cs
@@ -12,6 +12,8 @@
         public string this[string name]
             => this.cookies[name].Value;
 
+        public int Count => this.cookies.Count;
+
         public void Add(string name, string value)
             => this.cookies[name] = new Cookie(name, value);
 
