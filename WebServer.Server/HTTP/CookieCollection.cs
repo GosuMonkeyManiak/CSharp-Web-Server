56ff82edbaad7091157ee8b6fe316a1195197d61
Dimitar Kupanov <dimitarkupanov@gmail.com>
2022-01-20 19:46:50 +0200

    Separate a cookies in class and make a collection for easy use

---
+++ WebServer.Server/HTTP/CookieCollection.cs
@@ -0,0 +1,12 @@
+﻿namespace WebServer.Server.HTTP
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class CookieCollection
+    {
+    }
+}
